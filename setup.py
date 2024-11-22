8fe74158c0ef41d5ac9d361c85328d98253e933a
Saurav Sabu <59791440+saurav-sabu@users.noreply.github.com>
2024-11-22 21:16:29 +0000

    project structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = "LawClerk AI",
+    version = "0.0.0",
+    author = "Saurav Sabu",
+    author_email="saurav.sabu9@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
